dc2bdec3650327ef3176b7f14a57169a6e191c43
Adrian <adrian@ganseforth.me>
2023-09-08 13:00:38 +0200

    added services added transactional behaviour to services

---
+++ src/main/java/com/health/sugar/lf10sugarhealth/common/exceptions/MembershipNotFoundException.java
@@ -0,0 +1,7 @@
+package com.health.sugar.lf10sugarhealth.common.exceptions;
+
+public class MembershipNotFoundException extends Exception{
+    public MembershipNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/health/sugar/lf10sugarhealth/controller/MemberController.java
+++ src/main/java/com/health/sugar/lf10sugarhealth/controller/MemberController.java
@@ -1,11 +1,14 @@
 package com.health.sugar.lf10sugarhealth.controller;
 
+import com.health.sugar.lf10sugarhealth.common.exceptions.EmptyResponseException;
+import com.health.sugar.lf10sugarhealth.common.exceptions.MemberNotFoundException;
+import com.health.sugar.lf10sugarhealth.common.exceptions.MembershipNotFoundException;
 import com.health.sugar.lf10sugarhealth.model.Member;
 import com.health.sugar.lf10sugarhealth.model.MembershipStatus;
 import com.health.sugar.lf10sugarhealth.model.Profile;
-import com.health.sugar.lf10sugarhealth.repository.MemberRepository;
-import com.health.sugar.lf10sugarhealth.repository.MembershipStatusRepository;
-import com.health.sugar.lf10sugarhealth.repository.ProfileRepository;
+import com.health.sugar.lf10sugarhealth.service.MemberService;
+import com.health.sugar.lf10sugarhealth.service.MembershipStatusService;
+import com.health.sugar.lf10sugarhealth.service.ProfileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -24,13 +25,13 @@ import java.util.UUID;
 public class MemberController {
 
     @Autowired
-    MemberRepository memberRepository;
+    MemberService memberService;
 
     @Autowired
-    MembershipStatusRepository membershipStatusRepository;
+    ProfileService profileService;
 
     @Autowired
-    ProfileRepository profileRepository;
+    MembershipStatusService membershipStatusService;
 
     Logger logger = LoggerFactory.getLogger(MemberController.class);
 
@@ -38,13 +39,13 @@ public class MemberController {
     @GetMapping("/")
     public ResponseEntity<List<Member>> getAll() {
         try {
-            List<Member> members = new ArrayList<>(memberRepository.findAll());
-
-            if (members.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
+            List<Member> members = memberService.getAllMember();
 
             return new ResponseEntity<>(members, HttpStatus.OK);
+
+        } catch (EmptyResponseException emptyResponseException) {
+            logger.error(emptyResponseException.getMessage());
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
             logger.error(e.getMessage());
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -54,12 +55,13 @@ public class MemberController {
     @GetMapping("/{id}")
     public ResponseEntity<Member> getById(@PathVariable("id") UUID id) {
         try {
-            Optional<Member> memberOptional = memberRepository.findById(id);
+            Member member = memberService.getMemberById(id);
 
-            return memberOptional
-                    .map(member -> new ResponseEntity<>(member, HttpStatus.OK))
-                    .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+            return new ResponseEntity<>(member, HttpStatus.OK);
 
+        } catch (MemberNotFoundException memberNotFoundException) {
+            logger.error(memberNotFoundException.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } catch (Exception e) {
             logger.error(e.getMessage());
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -69,12 +71,12 @@ public class MemberController {
     @GetMapping("/{id}/membership")
     public ResponseEntity<MembershipStatus> getMembershipById(@PathVariable("id") UUID id) {
         try {
-            Optional<MembershipStatus> membershipStatusOptional = membershipStatusRepository.findByMemberId(id);
+            MembershipStatus membershipStatus = membershipStatusService.getMembershipStatusById(id);
 
-            return membershipStatusOptional
-                    .map(membershipStatus -> new ResponseEntity<>(membershipStatus, HttpStatus.OK))
-                    .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-
+            return new ResponseEntity<>(membershipStatus, HttpStatus.OK);
+        } catch (MembershipNotFoundException membershipNotFoundException) {
+            logger.error(membershipNotFoundException.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } catch (Exception e) {
             logger.error(e.getMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -82,15 +84,14 @@ public class MemberController {
     }
 
     @GetMapping("/{id}/profiles")
-    public ResponseEntity<List<Profile>> getProfileByMember(@PathVariable("id") UUID id) {
+    public ResponseEntity<List<Profile>> getProfileByMember(@PathVariable("id") UUID member_id) {
         try {
-            List<Profile> profiles = profileRepository.findAllByMemberId(id);
-
-            if (profiles.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
+            List<Profile> profiles = profileService.getProfilesByMemberId(member_id);
 
             return new ResponseEntity<>(profiles, HttpStatus.OK);
+        } catch (EmptyResponseException emptyResponseException) {
+            logger.error(emptyResponseException.getMessage());
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
             logger.error(e.getMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -98,9 +99,9 @@ public class MemberController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Member> deleteById(@PathVariable("id") UUID id) {
+    public ResponseEntity<Member> deleteById(@PathVariable("id") UUID member_id) {
         try {
-            memberRepository.deleteById(id);
+            memberService.deleteMember(member_id);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
             logger.error(e.getMessage());
@@ -111,23 +112,14 @@ public class MemberController {
     @PostMapping("/")
     public ResponseEntity<Member> create(@RequestBody Member memberRequestBody) {
         try {
-            MembershipStatus membershipStatus = membershipStatusRepository.save(new MembershipStatus());
-
-            Member member = memberRepository.save(
-                    new Member(memberRequestBody.getDisplayName(), membershipStatus, memberRequestBody.getLogin_uid()));
-
-            membershipStatus.setMember(member);
-
-            membershipStatusRepository.save(membershipStatus);
+            Member member = memberService.createMember(memberRequestBody.getDisplayName(), memberRequestBody.getLogin_uid());
 
             return new ResponseEntity<>(member, HttpStatus.CREATED);
 
-        } catch (DataIntegrityViolationException dve)
-        {
+        } catch (DataIntegrityViolationException dve) {
             logger.error("Ein Nutzer mit diesem Login-Konto existiert bereits.");
             return new ResponseEntity<>(null, HttpStatus.CONFLICT);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             logger.error(e.getMessage());
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
