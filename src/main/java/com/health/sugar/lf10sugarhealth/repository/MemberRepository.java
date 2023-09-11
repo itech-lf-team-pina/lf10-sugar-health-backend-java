693cb3cdb557a0fb02ee268c8961e33082a30942
Adrian <adrian@ganseforth.me>
2023-09-11 12:39:30 +0200

    added response when a member is already registered

--- src/main/java/com/health/sugar/lf10sugarhealth/repository/MemberRepository.java
+++ src/main/java/com/health/sugar/lf10sugarhealth/repository/MemberRepository.java
@@ -2,8 +2,12 @@ package com.health.sugar.lf10sugarhealth.repository;
 
 import com.health.sugar.lf10sugarhealth.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface MemberRepository extends JpaRepository<Member, UUID> {
+    @Query(value = "SELECT m from Member m WHERE m.login_uid = :uid")
+    Optional<Member> findMemberByLoginUid(String uid);
 }
